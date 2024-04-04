6659d94559fcebf20a7b7b1b0b53b5262cbdc847
Jonathan Costa <jonathanduartecosta@gmail.com>
2024-04-04 10:21:14 -0300

    estruturando o componente com props corretamente

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,14 @@
 import logo from '../assets/logo-event-icon.svg'
+import { NavLink } from './nav-link'
 
 export function Header() {
   return (
     <div className='flex items-center gap-5 py-2'>
       <img src={logo} alt="logo do evento" />
+
       <nav className='flex items-center gap-5'>
-        <a href="" className='font-medium text-sm text-zinc-300'>Eventos</a>
-        <a href="" className='font-medium text-sm'>Participantes</a>
+        <NavLink title='' href="#" >Eventos</NavLink>
+        <NavLink title='' href="#1" >Participantes</NavLink>
       </nav>
     </div>
   )
