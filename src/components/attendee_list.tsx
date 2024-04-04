0aad016d27aeb0570710977cf7e10a585b5dda6c
Jonathan Costa <jonathanduartecosta@gmail.com>
2024-04-04 09:58:31 -0300

    estrut e estilo da tabela

--- src/components/attendee_list.tsx
+++ src/components/attendee_list.tsx
@@ -1,12 +1,79 @@
-import { Search } from 'lucide-react'
+// ícones importados
+import { Search, MoreHorizontal, ChevronsLeft, ChevronLeft, ChevronsRight, ChevronRight } from 'lucide-react'
 
 export function AttendeeList() {
   return (
-    <div className="flex items-center gap-3">
-      <h1 className="text-2xl font-bold">Participantes</h1>
-      <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
-        <Search className='size-4 text-orange-600' />
-        <input className="bg-transparent flex-1 outline-none" placeholder="Buscar participante..." />
+    <div className='flex flex-col gap-4'>
+      <div className="flex items-center gap-3">
+        <h1 className="text-2xl font-bold">Participantes</h1>
+        <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
+          <Search className='size-4 text-orange-600' />
+          <input className="bg-transparent flex-1 outline-none border-0 p-0" placeholder="Buscar participante..." />
+        </div>
+      </div>
+
+      {/* INICIO DA TABELA */}
+      <div className='border border-white/10 rounded-lg'>
+        <table className='w-full'>
+
+          {/* CABEÇALHO */}
+          <thead>
+            <tr className='border-b border-white/10'>
+              <th style={{ width: 48 }} className="py-3 px-4 text-sm font-semibold text-left">
+                <input className='size-4 bg-black/20 rounded border-white/10 accent-orange-400' type="checkbox" />
+              </th>
+              <th className="py-3 px-4 text-sm font-semibold text-left">Código</th>
+              <th className="py-3 px-4 text-sm font-semibold text-left">Participante</th>
+              <th className="py-3 px-4 text-sm font-semibold text-left">Data de incrição</th>
+              <th className="py-3 px-4 text-sm font-semibold text-left">Data do check-in</th>
+              <th style={{ width: 64 }} className="py-3 px-4 text-sm font-semibold text-left"></th>
+            </tr>
+          </thead>
+
+          {/* LINHAS */}
+          <tbody>
+            <tr className='border-b border-white/10 hover:bg-white/5'>
+              <td className="py-3 px-4 text-sm text-zinc-300"><input className='size-4 bg-black/20 rounded border-white/10 accent-orange-400' type="checkbox" /></td>
+              <td className="py-3 px-4 text-sm text-zinc-300">152489</td>
+              <td className="py-3 px-4 text-sm text-zinc-300">
+                <div className='flex flex-col gap-1'>
+                  <span className='font-semibold text-white'>Jonathan Costa</span>
+                  <span>teste@teste.com</span>
+                </div>
+              </td>
+              <td className="py-3 px-4 text-sm text-zinc-300">7 dias atrás</td>
+              <td className="py-3 px-4 text-sm text-zinc-300">2 dias atrás</td>
+              <td className="py-3 px-4 text-sm text-zinc-300">
+                <button className='bg-black/20 border border-white/10 rounded-md p-1.5'>
+                  <MoreHorizontal className='size-4' />
+                </button>
+              </td>
+            </tr>
+          </tbody>
+
+          {/* RODAPÉ */}
+          <tfoot>
+            <tr>
+              <td className="py-3 px-4 text-sm text-zinc-300" colSpan={3}>Mostrando 10 de 228 itens</td>
+              <td className="py-3 px-4 text-sm text-zinc-300 text-right" colSpan={3}>
+                <div className='inline-flex items-center gap-8'>
+                  <span>Página 1 de 23</span>
+                  <div className='flex gap-1.5'>
+                    <button className='bg-black/20 border border-white/10 rounded-md p-1.5'>
+                      <ChevronsLeft className='size-4' />
+                    </button> <button className='bg-black/20 border border-white/10 rounded-md p-1.5'>
+                      <ChevronLeft className='size-4' />
+                    </button> <button className='bg-black/20 border border-white/10 rounded-md p-1.5'>
+                      <ChevronRight className='size-4' />
+                    </button> <button className='bg-black/20 border border-white/10 rounded-md p-1.5'>
+                      <ChevronsRight className='size-4' />
+                    </button>
+                  </div>
+                </div>
+              </td>
+            </tr>
+          </tfoot>
+        </table>
       </div>
     </div>
 
